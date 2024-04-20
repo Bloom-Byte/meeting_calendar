8c9c806feb3bdb3eff36d3b3e5da6f491524c1c5
ti-oluwa <105179638+ti-oluwa@users.noreply.github.com>
2024-04-20 12:54:44 +0100

    Update

--- core/views.py
+++ core/views.py
@@ -1,11 +1,17 @@
+from typing import Any
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
 from django.views import generic
-
+from users.decorators import redirect_authenticated
 
 
 class IndexView(generic.TemplateView):
     template_name = "core/index.html"
     http_method_names = ["get"]
 
+    @redirect_authenticated("dashboard:dashboard")
+    def get(self, request: HttpRequest, *args: Any, **kwargs: Any) -> HttpResponse:
+        return super().get(request, *args, **kwargs)
 
 
 index_view = IndexView.as_view()
