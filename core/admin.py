0e57edc0ac1c78d9733fe12a16f79a2c18878a74
ti-oluwa <105179638+ti-oluwa@users.noreply.github.com>
2024-04-22 09:06:28 +0100

    Updated

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.models import Group
-from rest_framework_api_key.models import APIKey
-
-admin.site.unregister(APIKey)
-admin.site.unregister(Group)
-
